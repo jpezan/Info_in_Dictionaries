8f6a88a9f71624ee0b5c9905e57dd85470380d3a
Jacqueline Pezan <71908081+jpezan@users.noreply.github.com>
2021-03-01 20:42:25 -0600

    Add files via upload

---
+++ Student_Dictionary.py
@@ -0,0 +1,20 @@
+print("Jacqueline's Dictionary")
+
+mydict = {"name": "Jacqueline",
+           "age": 21,
+           "location": "Chicago",
+           "hobbies": [
+               "yoga", 
+               "swimming", 
+               "reading", 
+               "building PCs"], 
+           "waketime": {
+               "mon": 7, 
+               "tues": 7, 
+               "wed": 7, 
+               "thurs": 7, 
+               "fri": 8}
+}
+print(mydict)
+print(mydict["wake up time"]["mon"])
+
